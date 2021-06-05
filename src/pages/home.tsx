6dfeb12ca95e2d5e57f1b038ca674af62d34eb63
MinChan Lee <shigatsu970704@gmail.com>
2021-06-05 15:04:28 +0900

    09.Nweet Form & Fire Store Set Up

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Home = () => <span>Home</span>;
+export const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={nweet}
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="Nweet" onChange={onChange} />
+      </form>
+    </div>
+  );
+};
