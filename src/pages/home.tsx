fc9a0fc0f885b29d795a6d78d6cf66e7030d19de
MinChan Lee <shigatsu970704@gmail.com>
2021-06-05 15:59:12 +0900

    11.Nweet Collection Get

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,12 @@
 // import { dbService } from "fb";
 import { dbService } from "fb";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState<
+    { id: string; data?: string; nweet?: string }[]
+  >([]);
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -21,6 +24,21 @@ export const Home = () => {
     setNweet(value);
   };
 
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((current) => [nweetObject, ...current]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+  console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -33,6 +51,11 @@ export const Home = () => {
         />
         <input type="submit" value="Nweet" onChange={onChange} />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>{nweet.nweet}</div>
+        ))}
+      </div>
     </div>
   );
 };
