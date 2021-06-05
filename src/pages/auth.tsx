5f41d635774280c9bb528c155dc0c14a82b2b4a9
MinChan Lee <shigatsu970704@gmail.com>
2021-06-05 11:02:12 +0900

    04.Login Form

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,3 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Auth = () => <span>Auth</span>;
+export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Email"
+          required
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="Password"
+          required
+          onChange={onChange}
+        />
+        <button>Log In</button>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
